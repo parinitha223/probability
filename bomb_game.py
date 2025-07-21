a098df39c187a72575095b939859de21c3589c0e
parinitha223 <parinithasrib223@gmail.com>
2025-07-21 12:04:29 +0530

    Create bomb_game.py

---
+++ bomb_game.py
@@ -0,0 +1,19 @@
+import random
+Boxes=[1,2,3,4,5]
+entry_fee=5
+n=1000   #no.of players
+gain_amount=entry_fee*n
+given_amount=0
+for i in range(n):
+    bomb=random.choice(Boxes)
+    open=random.choice(Boxes)
+    if bomb==open:
+       given_amount=0
+    else:
+        given_amount+=10
+if(gain_amount>given_amount):
+    print("you got profit by ", gain_amount - given_amount)
+elif (gain_amount<given_amount):
+    print("you got loss by ", given_amount - gain_amount)
+else:
+    print("you got no profit no loss")
