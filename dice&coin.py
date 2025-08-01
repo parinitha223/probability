5792d848319579aca1d4ab526cfd6cc6944f07cd
parinitha223 <parinithasrib223@gmail.com>
2025-08-01 06:35:54 +0530

    dice&coin.py

--- dice&coin.py
+++ dice&coin.py
@@ -1,6 +1,7 @@
+#printing the sample space of the event when we roll the dice and toss the coin at the same time
 import random
-dice=[1,2,3,4,5,6]
-coin=['h','t']
+dice=[1,2,3,4,5,6]    #virtual dice
+coin=['h','t']        #virtual coin
 for i in dice:
     for j in coin:
-        print(str(i)+j)
+        print(str(i)+j)        #printing each element of sample space
